65c99f2896abfc58ac2d64a2191ba2654aaada09
Seunghwan <hetzer12@icloud.com>
2020-08-16 14:47:26 +0900

    Add body-parser

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
 const app = express()
 const port = 3000 // 포트 설정(웹 서버 기본 포트는 80)
 
+// URL 분석
+app.use(bodyParser.urlencoded({ extended: true }))
+// JSON 분석
+app.use(bodyParser.json())
+
 // 몽고디비 연결 및 설정
-mongoose.connect('mongodb+srv://<username>:<password>@boiler-plate-a9iwf.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect('Your MongoDB Connection Code', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -12,6 +19,18 @@ mongoose.connect('mongodb+srv://<username>:<password>@boiler-plate-a9iwf.mongodb
 }).then(() => console.log("MongoDB Connected.")) // 연결 성공 시
     .catch(err => console.log(err)) // 연결 실패 시
 
+// localhost:3000으로 접속 시
 app.get('/', (req, res) => res.send('Hello World!'))
+
+// 회원 가입 정보를 데이터베이스에 저장
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({ success: true })
+    })
+})
 
 app.listen(port, () => console.log(`port is ${port}`))
