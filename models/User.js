e4e6954c190d67d2b8cca09eac0b2f12fbe0e8f5
Seunghwan <hetzer12@icloud.com>
2020-08-17 19:12:46 +0900

    Add bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
     name: {
@@ -25,6 +28,25 @@ const userSchema = mongoose.Schema({
         type: Number,
     }
 })
+
+// 비밀번호 암호화
+userSchema.pre('save', function (next) {
+    if (this.isModified('password')) {
+        // 비밀번호 가져옴
+        const myPlaintextPassword = this.password
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            // 에러 리턴
+            if (err) return next(err)
+            bcrypt.hash(myPlaintextPassword, salt, (err, hash) => {
+                if (err) return next(err)
+                // 비밀번호를 hash로 변경
+                this.password = hash
+                next()
+            })
+        })
+    }
+})
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
