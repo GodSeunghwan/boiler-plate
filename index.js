d0ccb761d95d43efa9e6018b52f79f1401d58bb8
Seunghwan <hetzer12@icloud.com>
2020-08-20 11:42:31 +0900

    로그인 기능 추가/jwt 발급 기능 추가

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const config = require('./config/key')
 const { User } = require('./models/User')
 const app = express()
@@ -10,6 +11,8 @@ const port = 3000 // 포트 설정(웹 서버 기본 포트는 80)
 app.use(bodyParser.urlencoded({ extended: true }))
 // JSON 분석
 app.use(bodyParser.json())
+
+app.use(cookieParser())
 
 // 몽고디비 연결 및 설정
 mongoose.connect(config.mongoURI, {
@@ -31,6 +34,33 @@ app.post('/register', (req, res) => {
     user.save((err, userInfo) => {
         if (err) return res.json({ success: false, err })
         return res.status(200).json({ success: true })
+    })
+})
+
+// 로그인
+app.post('/login', (req, res) => {
+    // 데이터베이스에 이메일이 있는지 확인
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "User not found."
+            })
+        }
+        // 이메일이 있다면 비밀번호가 맞는지 확인
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) {
+                return res.json({
+                    loginSuccess: false,
+                    message: "Wrong password"
+                })
+            }
+            //비밀번호가 맞다면 토큰 생성
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err)
+                res.cookie("x_auth", user.token).status(200).json({ loginSuccess: true, userId: user._id })
+            })
+        })
     })
 })
 
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const saltRounds = 10
 
@@ -12,9 +13,10 @@ const userSchema = mongoose.Schema({
         type: String,
         trim: true,
         unique: 1,
-    }, password: {
+    },
+    password: {
         type: String,
-        maxlength: 50,
+        maxlength: 100,
     },
     role: {
         type: Number,
@@ -31,21 +33,43 @@ const userSchema = mongoose.Schema({
 
 // 비밀번호 암호화
 userSchema.pre('save', function (next) {
-    if (this.isModified('password')) {
+    const user = this
+    if (user.isModified('password')) {
         // 비밀번호 가져옴
-        const myPlaintextPassword = this.password
+        const myPlaintextPassword = user.password
         bcrypt.genSalt(saltRounds, (err, salt) => {
             // 에러 리턴
             if (err) return next(err)
             bcrypt.hash(myPlaintextPassword, salt, (err, hash) => {
                 if (err) return next(err)
                 // 비밀번호를 hash로 변경
-                this.password = hash
+                user.password = hash
                 next()
             })
         })
+    } else {
+        next()
     }
 })
+
+userSchema.methods.comparePassword = function (myPlaintextPassword, callback) {
+    bcrypt.compare(myPlaintextPassword, this.password, (err, isMatch) => {
+        if (err) return callback(err)
+        callback(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function (callback) {
+    const user = this
+    // JWT를 이용하여 토큰 생성
+    const token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    user.token = token
+    user.save((err, user) => {
+        if (err) return callback(err)
+        callback(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
