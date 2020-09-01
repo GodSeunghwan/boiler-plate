8d192fe1869c271403cff2983587f100f1fc31cd
Seunghwan <hetzer12@icloud.com>
2020-09-01 17:54:14 +0900

    로그인 페이지 및 로그인 기능 추가

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { loginUser } from '../../../_actions/user_action'
 
-function LoginPage() {
+function LoginPage(props) {
+    const dispatch = useDispatch()
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandle = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+    const onPasswordHandle = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+    const onSubmitHandle = (event) => {
+        event.preventDefault()
+        let body = {
+            email: Email,
+            password: Password
+        }
+        dispatch(loginUser(body))
+        .then(res => {
+            if(res.payload.loginSuccess) {
+                props.history.push('/')
+            } else {
+                alert("Error")
+            }
+        })
+        
+    }
+
     return (
-        <div>LoginPage</div>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
+            <form
+                style={{ display: 'flex', flexDirection: 'column' }}
+                onSubmit={onSubmitHandle}>
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandle} />
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandle} />
+                <br />
+                <input type="submit" value="로그인" />
+            </form>
+        </div>
     )
 }
 
