349efb3635c2c7f3b0f0aa8a4cd9ba1e4d1fdb66
Seunghwan <hetzer12@icloud.com>
2020-09-10 16:05:30 +0900

    로그아웃 기능 추가

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import axios from 'axios'
 
-function LandingPage() {
+function LandingPage(props) {
+    const logoutHandle = () => {
+        axios.get('api/users/logout')
+            .then(res => {
+                if(res.data.success){
+                    props.history.push("/login")
+                } else {
+                    alert("로그아웃 실패")
+                }
+            })
+    }
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
             <h2>시작 페이지</h2>
+            <input onClick={logoutHandle} type="button" value="로그아웃" />
         </div>
     )
 }
